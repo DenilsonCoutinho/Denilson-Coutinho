a07f183e564d878caedabc77b2849fac7796b5b3
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2026-03-19 18:04:18 -0300

    melhorias

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    allowedHosts: [
+      "206b-2804-7f5-921e-eeee-846-fe4e-96f6-47b1.ngrok-free.app"
+    ]
+  },
   plugins: [
     tailwindcss(),
   ],
